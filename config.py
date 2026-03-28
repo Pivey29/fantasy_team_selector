51c2c4262cb29a9efed18e2bf41909f30b2dee1b
PeterIvey <peteri@advanceguidance.com>
2026-03-28 14:37:10 +0200

    use Scores v2

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ DRAFT_OPEN = False
 TOURNAMENT_NAME = "2026 Fantasy Regionals"
 RANKING_DATA = "data/test.csv"
 RESULTS_TEAMS_TAB = "Sheet1"
-RESULTS_SCORES_TAB = "Scores"
+RESULTS_SCORES_TAB = "Scores v2"
