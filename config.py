a341c34a7df4eed5b17e0616db01fa0a5e9006c0
PeterIvey <peteri@advanceguidance.com>
2026-04-24 21:12:35 +0200

    live

--- config.py
+++ config.py
@@ -56,11 +56,11 @@ def get_now():
 # --- TOURNAMENT MILESTONES ---
 RANKING_END_DT = datetime(2026, 4, 18, 18, 0, tzinfo=SAST)
 DRAFT_END_DT   = datetime(2026, 4, 24, 19, 0, tzinfo=SAST)
-TOURNAMENT_START_DT = datetime(2026, 4, 25, 8, 0, tzinfo=SAST) # Morning of Day 1
+TOURNAMENT_START_DT = datetime(2026, 4, 24, 19, 0, tzinfo=SAST) # Morning of Day 1
 
 # --- 3. STAGE AUTOMATION ---
 # Set to "RATINGS", "DRAFT", "LIVE" to override, or None for Auto-mode
-MANUAL_STAGE = None
+MANUAL_STAGE = "LIVE"
 
 def get_current_stage():
     if MANUAL_STAGE:
